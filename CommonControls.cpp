9fc4462679acd631b9813435728c5699add5ec9d
sfrwmaker <sfrwmaker@gmail.com>
2018-10-08 16:15:57 +0300

    Add files via upload

--- CommonControls.cpp
+++ CommonControls.cpp
@@ -4,213 +4,257 @@
 #include "CommonControls.h"
 
 //------------------------------------------ backlight of the LCD display (lite version) ----------------------
-void BL::init(void) {
+BL::BL(uint8_t sensorPIN, uint8_t lightPIN, uint8_t start_brightness) {
+    sensor_pin          = sensorPIN;
+    led_pin             = lightPIN;
+    default_brightness  = start_brightness;
+    b_night             =  50;
+    daily_brightness    = 150;
+    b_day               = 500;
+}
 
-  pinMode(led_pin, OUTPUT);
-  pinMode(sensor_pin, INPUT);
-  int light = analogRead(sensor_pin);
-  emp = 0;
-  brightness = new_brightness = default_brightness;
-  checkMS = ch_step = 0;
-  use_local_time = false;
-  automatic = true;
-  nightly_brightness = 50;
-  evening = morning = 0;                            // This value will be overwritten by config
-  adjust();
+void BL::init(void) {
+    pinMode(led_pin, OUTPUT);
+    pinMode(sensor_pin, INPUT);
+    emp = 0;
+    brightness = new_brightness = default_brightness;
+    check_ms = ch_step = 0;
+    use_local_time = false;
+    automatic = true;
+    nightly_brightness = 50;
+    evening = morning = 0;                          // This value will be overwritten by config
+    adjust();
 }
 
 int BL::empAverage(int v) {
-  long nv = v *emp_k;
-  int round_v = emp_k >> 1;
-  emp += (nv - emp + round_v) / emp_k;
-  int r = (emp + round_v) / emp_k;
-  return r;
+    long nv = v *emp_k;
+    int round_v = emp_k >> 1;
+    emp += (nv - emp + round_v) / emp_k;
+    int r = (emp + round_v) / emp_k;
+    return r;
 }
   
 void BL::adjust(void) {
-
-  if (!automatic) return;
+    if (!automatic) return;
   
-  uint32_t ms = millis();
-  if ((ms > ch_step) && (new_brightness != brightness)) {
-    if (new_brightness > brightness) ++brightness; else --brightness;
-    analogWrite(led_pin, brightness);
-    ch_step = ms + ch_period;
-  }
+    uint32_t ms = millis();
+    if ((ms > ch_step) && (new_brightness != brightness)) {
+        if (new_brightness > brightness) ++brightness; else --brightness;
+        analogWrite(led_pin, brightness);
+        ch_step = ms + ch_period;
+    }
 
-  if (ms < checkMS) return;
-  checkMS = ms + period;
+    if (ms < check_ms) return;
+    check_ms = ms + period;
 
-  // Turn off the backlight at night
-  if (isDark()) {
-    new_brightness = nightly_brightness;
-    return;
-  }
+    // Turn off the backlight at night
+    if (isDark()) {
+        new_brightness = nightly_brightness;
+        return;
+    }
   
-  int light = analogRead(sensor_pin);
+    uint16_t light = analogRead(sensor_pin);
 
-  light = empAverage(light);
-  if (light < b_night) {
-    new_brightness = nightly_brightness;
-    return;
-  }
+    light = empAverage(light);
+    if (light < b_night) {
+        new_brightness = nightly_brightness;
+        return;
+    }
 
-  if (off_daily && (light > b_day)) {
-    new_brightness = 0;
-    return;
-  }
+    if (off_daily && (light > b_day)) {
+        new_brightness = 0;
+        return;
+    }
 
-  light          = constrain(light, b_night, b_day);
-  new_brightness = map(light, b_night, b_day, nightly_brightness, daily_brightness);
-  new_brightness = constrain(new_brightness, nightly_brightness, daily_brightness);
+    light          = constrain(light, b_night, b_day);
+    new_brightness = map(light, b_night, b_day, nightly_brightness, daily_brightness);
+    new_brightness = constrain(new_brightness, nightly_brightness, daily_brightness);
 }
 
-void BL::setBrightness(byte b) {
-  brightness = b;
-  automatic = false;
-  analogWrite(led_pin, brightness);
+void BL::setBrightness(uint8_t b) {
+    brightness = b;
+    automatic = false;
+    analogWrite(led_pin, brightness);
 }
 
 void BL::turnAuto(bool a) {
-  automatic = a;
-  checkMS = 0;
-  if (a) adjust();    
+    automatic = a;
+    check_ms = 0;
+    if (a) adjust();    
 }
 
-void BL::setLimits(uint16_t dark, uint16_t daylight, byte br_nightly, byte br_daily, bool offDaily) {
-  b_night             = dark;
-  b_day               = daylight;
-  daily_brightness    = br_daily;
-  nightly_brightness  = br_nightly;
-  off_daily           = offDaily;
+void BL::setLimits(uint16_t dark, uint16_t daylight, uint8_t br_nightly, uint8_t br_daily, bool offDaily) {
+    b_night             = dark;
+    b_day               = daylight;
+    daily_brightness    = br_daily;
+    nightly_brightness  = br_nightly;
+    off_daily           = offDaily;
 }
 
-void BL::setNightPeriod(byte Evening, byte Morning) { // Time in 10-minute intervals from midnight
-  if (Evening <= Morning) return;
-  if (Evening > 144)    return;
-  morning = Morning;
-  evening = Evening;
-  use_local_time = true;
+void BL::setNightPeriod(uint8_t Evening, uint8_t Morning) { // Time in 10-minute intervals from midnight
+    if (Evening <= Morning) return;
+    if (Evening > 144)      return;
+    morning = Morning;
+    evening = Evening;
+    use_local_time = true;
 }
 
 bool BL::isDark(void) {
+    if (use_local_time) {
+        long now_t = hour(); now_t *= 60;
+        now_t += minute();   now_t *= 60;
+        now_t += second();
+        long m = long(morning) * 600;
+        long e = long(evening) * 600;
+        return ((now_t < m) || (now_t >= e));
+    }
 
-  if (use_local_time) {
-    long now_t = hour(); now_t *= 60;
-    now_t += minute();   now_t *= 60;
-    now_t += second();
-    long m = long(morning) * 600;
-    long e = long(evening) * 600;
-    return ((now_t < m) || (now_t >= e));
-  }
-
-  long light = 0;
-  for (byte i = 0; i < 4; ++i) {
-    light += analogRead(sensor_pin);
-    delay(20);
-  }
-  light >>= 2;
-  return (light < b_night);
+    long light = 0;
+    for (uint8_t i = 0; i < 4; ++i) {
+        light += analogRead(sensor_pin);
+        delay(20);
+    }
+    light >>= 2;
+    return (light < b_night);
 }
 
 //------------------------------------------ class BUTTON ------------------------------------------------------
-void BUTTON::changeINTR(void) {                     // Interrupt function, called when the button status changed
-  
-  bool keyUp = digitalRead(buttonPIN);
-  unsigned long now_t = millis();
-  if (!keyUp) {                                     // The button has been pressed
-    if ((pt == 0) || (now_t - pt > overPress)) pt = now_t; 
-  } else {
-    if (pt > 0) {
-      if ((now_t - pt) < shortPress) mode = 1;      // short press
-        else mode = 2;                              // long press
-      pt = 0;
-    }
-  }
+BUTTON::BUTTON(uint8_t ButtonPIN, unsigned int timeout_ms) {
+    pt = tick_time = 0;
+    button_pin = ButtonPIN;
+    over_press = timeout_ms;
 }
 
-byte BUTTON::buttonCheck(void) {               // Check the button state, called each time in the main loop
+void BUTTON::changeINTR(void) {                     // Interrupt function, called when the button status changed
+    bool keyUp = digitalRead(button_pin);
+    unsigned long now_t = millis();
+    if (!keyUp) {                                   // The button has been pressed
+        if ((pt == 0) || (now_t - pt > over_press)) pt = now_t; 
+    } else {
+        if (pt > 0) {
+            if ((now_t - pt) < shortPress) mode = 1;// short press
+            else mode = 2;                          // long press
+            pt = 0;
+        }
+    }
+}
 
-  mode = 0;
-  bool keyUp = digitalRead(buttonPIN);         // Read the current state of the button
-  uint32_t now_t = millis();
-  if (!keyUp) {                                // The button is pressed
-    if ((pt == 0) || (now_t - pt > overPress)) pt = now_t;
-  } else {
-    if (pt == 0) return 0;
-    if ((now_t - pt) < bounce) return 0;
-    if ((now_t - pt) > shortPress)             // Long press
-      mode = 2;
-    else
-      mode = 1;
-    pt = 0;
-  } 
-  return mode;
+uint8_t BUTTON::buttonCheck(void) {                 // Check the button state, called each time in the main loop
+    mode = 0;
+    bool keyUp = digitalRead(button_pin);           // Read the current state of the button
+    uint32_t now_t = millis();
+    if (!keyUp) {                                   // The button is pressed
+        if ((pt == 0) || (now_t - pt > over_press)) pt = now_t;
+    } else {
+        if (pt == 0) return 0;
+        if ((now_t - pt) < bounce) return 0;
+        if ((now_t - pt) > shortPress)              // Long press
+            mode = 2;
+        else
+            mode = 1;
+        pt = 0;
+    } 
+    return mode;
 }
 
 bool BUTTON::buttonTick(void) {                     // When the button pressed for a while, generate periodical ticks
+    bool keyUp = digitalRead(button_pin);           // Read the current state of the button
+    uint32_t now_t = millis();
+    if (!keyUp && (now_t - pt > shortPress)) {      // The button have been pressed for a while
+        if (now_t - tick_time > tick_timeout) {
+            tick_time = now_t;
+            return (pt != 0);
+        }
+    } else {
+        if (pt == 0) return false;
+        tick_time = 0;
+    } 
+    return false;
+}
 
-  bool keyUp = digitalRead(buttonPIN);              // Read the current state of the button
-  uint32_t now_t = millis();
-  if (!keyUp && (now_t - pt > shortPress)) {        // The button have been pressed for a while
-    if (now_t - tickTime > tickTimeout) {
-       tickTime = now_t;
-       return (pt != 0);
+//------------------------------------------ class SWITCH ------------------------------------------------------
+SWITCH::SWITCH(uint8_t SwitchPIN) {
+    pt = 0;
+    switch_pin    = SwitchPIN;
+}
+
+void SWITCH::init(uint32_t switch_time) {
+    bounce = switch_time;
+    pinMode(switch_pin, INPUT_PULLUP);
+    mode = digitalRead(switch_pin);
+}
+
+bool SWITCH::status(void) {
+    bool sw_on = digitalRead(switch_pin);           // Read the current state of the switch
+    uint32_t now_t = millis();
+    if (mode == sw_on) {                            // The mode does not changed
+        return mode;
+    } else {                                        // The mode has been changed
+        if ((now_t - pt) < bounce) return mode;     // Prevent bouncing, return previous state
+        pt = now_t;
+        mode = sw_on;
     }
-  } else {
-    if (pt == 0) return false;
-    tickTime = 0;
-  } 
-  return false;
+    return mode;
 }
 
 //------------------------------------------ class ENCODER ------------------------------------------------------
-bool ENCODER::write(int16_t initPos) {
-  if ((initPos >= min_pos) && (initPos <= max_pos)) {
-    pos = initPos;
-    return true;
-  }
-  return false;
+ENCODER::ENCODER(uint8_t aPIN, uint8_t bPIN, int16_t initPos) {
+    pt = 0; m_pin = aPIN; s_pin = bPIN; pos = initPos;
+    min_pos = -32767; max_pos = 32766; ch_b = false; increment = 1;
+    changed = 0;
+    is_looped = false;
 }
 
-void ENCODER::reset(int16_t initPos, int16_t low, int16_t upp, byte inc, byte fast_inc, bool looped) {
-  min_pos = low; max_pos = upp;
-  if (!write(initPos)) initPos = min_pos;
-  increment = fast_increment = inc;
-  if (fast_inc > increment) fast_increment = fast_inc;
-  is_looped = looped;
+void ENCODER::init(void) {
+    pinMode(m_pin, INPUT_PULLUP);
+    pinMode(s_pin, INPUT_PULLUP);
+}
+
+bool ENCODER::write(int16_t initPos) {
+    if ((initPos >= min_pos) && (initPos <= max_pos)) {
+        pos = initPos;
+        return true;
+    }
+    return false;
+}
+
+void ENCODER::reset(int16_t initPos, int16_t low, int16_t upp, uint8_t inc, uint8_t fast_inc, bool looped) {
+    min_pos = low; max_pos = upp;
+    if (!write(initPos)) initPos = min_pos;
+    increment = fast_increment = inc;
+    if (fast_inc > increment) fast_increment = fast_inc;
+    is_looped = looped;
 }
 
 void ENCODER::changeINTR(void) {                    // Interrupt function, called when the channel A of encoder changed
-  
-  bool rUp = digitalRead(mPIN);
-  unsigned long now_t = millis();
-  if (!rUp) {                                       // The channel A has been "pressed"
-    if ((pt == 0) || (now_t - pt > overPress)) {
-      pt = now_t;
-      channelB = digitalRead(sPIN);
-    }
-  } else {
-    if (pt > 0) {
-      byte inc = increment;
-      if ((now_t - pt) < overPress) {
-        if ((now_t - changed) < fast_timeout) inc = fast_increment;
-        changed = now_t;
-        if (channelB) pos -= inc; else pos += inc;
-        if (pos > max_pos) { 
-          if (is_looped)
-            pos = min_pos;
-          else 
-            pos = max_pos;
+    bool rUp = digitalRead(m_pin);
+    unsigned long now_t = millis();
+    if (!rUp) {                                     // The channel A has been "pressed"
+        if ((pt == 0) || (now_t - pt > over_press)) {
+            pt = now_t;
+            ch_b = digitalRead(s_pin);
         }
-        if (pos < min_pos) {
-          if (is_looped)
-            pos = max_pos;
-          else
-            pos = min_pos;
+    } else {
+        if (pt > 0) {
+            uint8_t inc = increment;
+            if ((now_t - pt) < over_press) {
+                if ((now_t - changed) < fast_timeout) inc = fast_increment;
+                    changed = now_t;
+                    if (ch_b) pos -= inc; else pos += inc;
+                    if (pos > max_pos) { 
+                        if (is_looped)
+                            pos = min_pos;
+                        else 
+                            pos = max_pos;
+                    }
+                    if (pos < min_pos) {
+                        if (is_looped)
+                            pos = max_pos;
+                        else
+                            pos = min_pos;
+                    }
+                }
+                pt = 0; 
         }
-      }
-      pt = 0; 
     }
-  }
 }
--- CommonControls.h
+++ CommonControls.h
@@ -3,102 +3,97 @@
 
 //------------------------------------------ backlight of the LCD display (lite version) ----------------------
 class BL {
-  public:
-    BL(byte sensorPIN, byte lightPIN, byte start_brightness = 128) {
-      sensor_pin          = sensorPIN;
-      led_pin             = lightPIN;
-      default_brightness  = start_brightness;
-      b_night             =  50;
-      daily_brightness    = 150;
-      b_day               = 500;
-    }
-    void init(void);                                // Initialize the data
-    void adjust(void);                              // Automatically adjust the brightness
-    int  getSensorValue(void)                       { return analogRead(sensor_pin); }
-    void setBrightness(byte b);
-    void turnAuto(bool a);
-    void setLimits(uint16_t dark, uint16_t daylight, byte br_nightly, byte br_daily, bool offDaily = true);
-    void setNightPeriod(byte Evening, byte Morning);
-    bool isDark(void);                              // Whether it is night time or it is dark here
-  private:
-    int      empAverage(int v);                     // Exponential average value
-    byte     sensor_pin;                            // Light sensor pin
-    byte     led_pin;                               // Led PWM pin
-    bool     off_daily;                             // Whether turn the light daily
-    uint32_t checkMS;                               // Time in ms when the sensor was checked
-    uint32_t ch_step;                               // The time in ms when the brightness can be adjusted
-    bool     automatic;                             // Whether the backlight should be adjusted automatically
-    bool     use_local_time;                        // Whether to use local time to switch off the light nightly
-    byte     brightness;                            // The backlight brightness
-    byte     new_brightness;                        // The baclight brightness to set up
-    byte     evening, morning;                      // The time of evening and morning (in 10-minutes interval)
-    long     emp;                                   // Exponential average value
-    byte     default_brightness;                    // Default brightness of backlight
-    uint16_t b_night;                               // light sensor value of the night
-    uint16_t b_day;                                 // light sensor value of the day light
-    byte     daily_brightness;                      // The maximum brightness of backlight when light between b_night and b_day
-    byte     nightly_brightness;                    // The brightness to use nightly
-    const byte     emp_k = 8;                       // The exponential average coefficient
-    const uint16_t period  = 200;                   // The period in ms to check the photoresister
-    const uint16_t ch_period = 5;                   // The period to adjust brightness
+    public:
+        BL(uint8_t sensorPIN, uint8_t lightPIN, uint8_t start_brightness = 128);
+        void        init(void);                    // Initialize the data
+        void        adjust(void);                  // Automatically adjust the brightness
+        int         getSensorValue(void)           { return analogRead(sensor_pin); }
+        void        setBrightness(uint8_t b);
+        void        turnAuto(bool a);
+        void        setLimits(uint16_t dark, uint16_t daylight, uint8_t br_nightly, uint8_t br_daily, bool offDaily = true);
+        void        setNightPeriod(uint8_t Evening, uint8_t Morning);
+        bool        isDark(void);                   // Whether it is night time or it is dark here
+    private:
+        int         empAverage(int v);              // Exponential average value
+        uint8_t     sensor_pin;                     // Light sensor pin
+        uint8_t     led_pin;                        // Led PWM pin
+        bool        off_daily;                      // Whether turn the light daily
+        uint32_t    check_ms;                        // Time in ms when the sensor was checked
+        uint32_t    ch_step;                        // The time in ms when the brightness can be adjusted
+        bool        automatic;                      // Whether the backlight should be adjusted automatically
+        bool        use_local_time;                 // Whether to use local time to switch off the light nightly
+        uint8_t     brightness;                     // The backlight brightness
+        uint8_t     new_brightness;                 // The baclight brightness to set up
+        uint8_t     evening, morning;               // The time of evening and morning (in 10-minutes interval)
+        long        emp;                            // Exponential average value
+        uint8_t     default_brightness;             // Default brightness of backlight
+        uint16_t    b_night;                        // light sensor value of the night
+        uint16_t    b_day;                          // light sensor value of the day light
+        uint8_t     daily_brightness;               // The maximum brightness of backlight when light between b_night and b_day
+        uint8_t     nightly_brightness;             // The brightness to use nightly
+        const uint8_t       emp_k = 8;              // The exponential average coefficient
+        const uint16_t      period  = 200;          // The period in ms to check the photoresister
+        const uint16_t      ch_period = 5;          // The period to adjust brightness
 };
 
 //------------------------------------------ class BUTTON ------------------------------------------------------
 class BUTTON {
-  public:
-    BUTTON(byte ButtonPIN, unsigned int timeout_ms = 3000) {
-      pt = tickTime = 0;
-      buttonPIN = ButtonPIN;
-      overPress = timeout_ms;
-    }
-    void init(void) { pinMode(buttonPIN, INPUT_PULLUP); }
-    void setTimeout(uint16_t timeout_ms = 3000)    { overPress = timeout_ms; }
-    byte intButtonStatus(void)                     { byte m = mode; mode = 0; return m; }
-    void changeINTR(void);
-    byte buttonCheck(void);
-    bool buttonTick(void);
-  private:
-    volatile byte     mode;                         // The button mode: 0 - not pressed, 1 - pressed, 2 - long pressed
-    uint16_t          overPress;                    // Maximum time in ms the button can be pressed
-    volatile uint32_t pt;                           // Time in ms when the button was pressed (press time)
-    uint32_t          tickTime;                     // The time in ms when the button Tick was set
-    byte              buttonPIN;                    // The pin number connected to the button
-    const uint16_t    tickTimeout = 200;            // Period of button tick, while the button is pressed 
-    const uint16_t    shortPress  = 900;            // If the button was pressed less that this timeout, we assume the short button press
-    const byte        bounce      = 50;             // Bouncing timeout (ms)
+    public:
+        BUTTON(uint8_t ButtonPIN, unsigned int timeout_ms = 3000);
+        void        init(void)                      { pinMode(button_pin, INPUT_PULLUP); }
+        void        setTimeout(uint16_t to = 3000)  { over_press = to; }
+        uint8_t     intButtonStatus(void)                     { uint8_t m = mode; mode = 0; return m; }
+        void        changeINTR(void);
+        uint8_t     buttonCheck(void);
+        bool        buttonTick(void);
+    private:
+        uint32_t    tick_time;                      // The time in ms when the button Tick was set
+        uint8_t     button_pin;                     // The pin number connected to the button
+        uint16_t    over_press;                     // Maximum time in ms the button can be pressed
+        volatile uint8_t    mode;                   // The button mode: 0 - not pressed, 1 - pressed, 2 - long pressed
+        volatile uint32_t   pt;                     // Time in ms when the button was pressed (press time)
+        const uint16_t      tick_timeout = 200;     // Period of button tick, while the button is pressed 
+        const uint16_t      shortPress  = 900;      // If the button was pressed less that this timeout, we assume the short button press
+        const uint8_t       bounce      = 50;       // Bouncing timeout (ms)
+};
+
+//------------------------------------------ class SWITCH ------------------------------------------------------
+class SWITCH {
+    public:
+        SWITCH(uint8_t SwitchPIN);
+        void        init(uint32_t switch_time = 500);
+        bool        status(void);
+    private:
+        bool        mode;                           // The switch mode on (true)/off
+        uint16_t    over_press;                     // Maximum time in ms the button can be pressed
+        uint32_t    pt;                             // Time in ms when the switch status changed
+        uint8_t     switch_pin;                     // The pin number connected to the button
+        uint32_t    bounce      = 500;              // Status change interval (ms)
 };
 
 //------------------------------------------ class ENCODER ------------------------------------------------------
 class ENCODER {
-  public:
-    ENCODER(byte aPIN, byte bPIN, int16_t initPos = 0) {
-      pt = 0; mPIN = aPIN; sPIN = bPIN; pos = initPos;
-      min_pos = -32767; max_pos = 32766; channelB = false; increment = 1;
-      changed = 0;
-      is_looped = false;
-    }
-    void init(void) {
-      pinMode(mPIN, INPUT_PULLUP);
-      pinMode(sPIN, INPUT_PULLUP);
-    }
-    void    set_increment(byte inc)             { increment = inc; }
-    byte    get_increment(void)                 { return increment; }
-    int16_t read(void)                          { return pos; }
-    void    reset(int16_t initPos, int16_t low, int16_t upp, byte inc = 1, byte fast_inc = 0, bool looped = false);
-    bool    write(int16_t initPos);
-    void    changeINTR(void);
-  private:
-    int32_t           min_pos, max_pos;
-    volatile uint32_t pt;                           // Time in ms when the encoder was rotaded
-    volatile uint32_t changed;                      // Time in ms when the value was changed
-    volatile bool     channelB;
-    volatile int16_t  pos;                          // Encoder current position
-    byte              mPIN, sPIN;                   // The pin numbers connected to the main channel and to the socondary channel
-    bool              is_looped;                    // Whether the encoder is looped
-    byte              increment;                    // The value to add or subtract for each encoder tick
-    byte              fast_increment;               // The value to change encoder when in runs quickly
-    const uint16_t    fast_timeout = 300;           // Time in ms to change encoder quickly
-    const uint16_t    overPress = 1000;
+    public:
+        ENCODER(uint8_t aPIN, uint8_t bPIN, int16_t initPos = 0);
+        void        init(void);
+        void        set_increment(uint8_t inc)      { increment = inc; }
+        uint8_t     get_increment(void)             { return increment; }
+        int16_t     read(void)                      { return pos; }
+        void        reset(int16_t initPos, int16_t low, int16_t upp, uint8_t inc = 1, uint8_t fast_inc = 0, bool looped = false);
+        bool        write(int16_t initPos);
+        void        changeINTR(void);
+    private:
+        int32_t     min_pos, max_pos;
+        uint8_t     m_pin, s_pin;                     // The pin numbers connected to the main channel and to the socondary channel
+        bool        is_looped;                      // Whether the encoder is looped
+        uint8_t     increment;                      // The value to add or subtract for each encoder tick
+        uint8_t     fast_increment;                 // The value to change encoder when in runs quickly
+        volatile uint32_t   pt;                     // Time in ms when the encoder was rotaded
+        volatile uint32_t   changed;                // Time in ms when the value was changed
+        volatile bool       ch_b;
+        volatile int16_t    pos;                    // Encoder current position
+        const uint16_t      fast_timeout    = 300;  // Time in ms to change encoder quickly
+        const uint16_t      over_press      = 1000;
 };
 
 #endif
